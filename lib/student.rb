eb4446a91d22bfc5e54589db55d6907645906daa
Chris Philbin <chris@chrisphilbin.net>
2019-01-12 16:19:18 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require 'user.rb'
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(statement)
+    @knowledge << statement
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
