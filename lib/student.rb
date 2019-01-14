05003bb3d6ab48a9d67f9e542ea400b9ebd67852
Alua Baikadamova <abaikad00@gmail.com>
2019-01-14 15:43:41 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
-
+class Student<User
+def initialize
+    @knowledge = []
+    
+  end
+  
+  def learn(sent)
+    @knowledge.push(sent)
+  end
+  
+    def knowledge
+    @knowledge
+  end
 end
